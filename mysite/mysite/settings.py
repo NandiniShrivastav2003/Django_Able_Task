9fdd3ab26ceac2d2dcc0fce1b43a6435698a2d0f
Nandini Shrivastav <nandini.shrivastav_cs20@gla.ac.in>
2023-06-17 00:02:34 +0530

    Adding contatc page and storing contatc details in databse

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR:'danger'
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
